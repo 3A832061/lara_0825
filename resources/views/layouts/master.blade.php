25c8fff47463bca147d3ef6c82a1477ef9b5d623
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-11-03 10:22:29 +0800

    修改master.php(引入footer打錯)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -29,7 +28,7 @@
 
 <hr>
 
-    @yield('layouts.partials.footer')
+    @include('layouts.partials.footer')
 
 <!-- Bootstrap core JavaScript -->
     <script src="{{asset('js/jquery.min.js')}}"></script>
