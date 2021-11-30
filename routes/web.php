a1f4ef23cb1cdcc70fda3547910de45f725ddbdc
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-12-01 07:05:14 +0800

    以mass assignment方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Post::create([
+        'title' => 'test title',
+        'content' => 'test content',
+    ]);
     return view('welcome');
 });
 
