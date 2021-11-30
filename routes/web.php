04639f5d1e07db0cbb267d71bdcb26f2cd0a399d
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-12-01 07:24:13 +0800

    顯示所有資料

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,10 @@ Route::get('/', function () {
 
     //Post::destroy(2);
 
-    Post::destroy(3, 5, 7);
+    //Post::destroy(3, 5, 7);
+
+    $allPosts = Post::all();
+    dd($allPosts);
 
     return view('welcome');
 });
