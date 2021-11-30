1ca2497d3621a54326be59b2eaabc3fa55e21520
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-12-01 07:22:49 +0800

    刪除id=3,5,7的資料

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,9 @@ Route::get('/', function () {
     /*$post = Post::find(1);
     $post->delete();*/
 
-    Post::destroy(2);
+    //Post::destroy(2);
+
+    Post::destroy(3, 5, 7);
 
     return view('welcome');
 });
